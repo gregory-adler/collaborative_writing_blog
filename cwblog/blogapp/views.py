9b5ba5709916159a9f921e6057d9f0b66607b561
eugene-git <eluebke@emory.edu>
2015-01-26 12:40:16 -0500

    Created main view and main template

--- cwblog/blogapp/views.py
+++ cwblog/blogapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
-# Create your views here.
+from blogapp.models import Story, Submission
+
+
+def main(request):
+    # TODO: Need to get the submissions for each story and display them
+    stories = Story.objects.all().order_by('-date')
+    submissions = Submission.objects.all()
+    paginator = Paginator(stories, 1)
+
+    try:
+        page = int(request.GET.get("page", '1'))
+    except ValueError:
+        page = 1
+
+    try:
+        stories = paginator.page(page)
+    except (InvalidPage, EmptyPage):
+        stories = paginator.page(paginator.num_pages)
+
+    return render(request, 'blogapp/main.html', dict(stories=stories, submissions=submissions))
+
--- cwblog/cwblog/urls.py
+++ cwblog/cwblog/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from blogapp import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cwblog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+                       url(r"", views.main, name='main'),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
