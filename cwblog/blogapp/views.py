f77ef885d0f275b31abaa272ae5c260ec1c7d392
eugene-git <eluebke@emory.edu>
2015-02-02 13:56:39 -0500

    Login page created, but users can't log in yet

--- cwblog/blogapp/views.py
+++ cwblog/blogapp/views.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+
+from django.shortcuts import render, render_to_response
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.template import RequestContext
 from django.utils import timezone
-from blogapp.models import Story, Submission
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.views.generic.edit import CreateView
-from django.http import HttpResponseForbidden
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+from blogapp.models import Story, Submission
 
 
 def home(request):
@@ -68,35 +70,53 @@ def add_to_story(submission):
     submission.delete()
 
 
-class Register(CreateView):
+class RegisterView(CreateView):
     form_class = UserCreationForm
     model = User
     template_name = 'blogapp/register.html'
     success_url = '/'
 
 
-# class Login(AuthenticationForm):
-#
-#     def form_valid(self, form):
-#         username = form.cleaned_data['username']
-#         password = form.cleaned_data['password']
-#         user = authenticate(username=username, password=password)
-#
-#         if user is not None and user.is_active:
-#             login(self.request, user)
-#             return super(Register, self).form_valid(form)
-#         else:
-#             return self.form_invalid(form)
+def login(request):
+    context = RequestContext(request)
+
+    # If the request is a HTTP POST, try to pull out the relevant information.
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        username = request.POST['username']
+        password = request.POST['password']
+
+        # Use Django's machinery to attempt to see if the username/password
+        # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+
+        # If we have a User object, the details are correct.
+        # If None (Python's way of representing the absence of a value), no user
+        # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in.
+                # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect('/')
+            else:
+                # An inactive account was used - no logging in!
+                return HttpResponse("Your account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            # print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    # The request is not a HTTP POST, so display the login form.
+    # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the
+        # blank dictionary object...
+        return render_to_response('blogapp/login.html', {}, context)
 
 
-def login_page(request):
-
-   return render(request, 'blogapp/login.html', dict())
-
-
-def register_user(request):
-
-    return HttpResponseForbidden()
 
 def logout(request):
 
--- cwblog/cwblog/urls.py
+++ cwblog/cwblog/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from blogapp import views
-from blogapp.views import Register
+from blogapp.views import RegisterView
 
 urlpatterns = patterns('',
                         url(r'^$', views.home, name='home'),
                         url(r'^(?P<page>\d+)$', views.main, name='main'),
                         url(r'^admin/', include(admin.site.urls)),
-                        url(r'^register/', Register.as_view(), name='register'),
+                        url(r'^register/', RegisterView.as_view(), name='register'),
+                        url(r'^login/', views.login, name='login'),
                         url(r'^(?P<page>\d+)/submission/$', views.post_submission, name= "submission"),
                         url(r'^(?P<page>\d+)/submission/(?P<submission_id>\d+)/like/$', views.like_button, name='like'),
                         url(r'^(?P<page>\d+)/submission/(?P<submission_id>\d+)/dislike/$', views.dislike_button, name='dislike')
