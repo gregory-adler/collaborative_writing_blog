f2f0a29a717986f14fea2a538a28d10f3735fd95
eugene-git <eluebke@emory.edu>
2015-02-02 01:29:56 -0500

    Changed time format; formatting

--- cwblog/blogapp/views.py
+++ cwblog/blogapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
-from datetime import datetime
+from django.utils import timezone
 from blogapp.models import Story, Submission
 from django.shortcuts import redirect
 
+
 def home(request):
-    return redirect(main,1)
+    return redirect(main, 1)
+
 
 def main(request, page):
 
@@ -23,41 +25,43 @@ def main(request, page):
 
 def post_submission(request, page):
 
-    #page = (int) page
+    # page = (int) page
 
     if request.POST["submission"]== "":
         pass
     else:
         new_submission = Submission() 
-        new_submission.text= request.POST["submission"]
-        new_submission.story= Story.objects.get(pk=page)
-        new_submission.date=  datetime.now()
+        new_submission.text = request.POST["submission"]
+        new_submission.story = Story.objects.get(pk=page)
+        new_submission.date = timezone.now()
         new_submission.save()
 
-    stories = Story.objects.all().order_by('-date')
-    submissions = Submission.objects.all()
+    # stories = Story.objects.all().order_by('-date')
+    # submissions = Submission.objects.all()
 
     return redirect(main, page)
 
 
 def like_button(request, page, submission_id):
     submission = Submission.objects.get(pk=submission_id)
-    submission.votes=submission.votes+1
+    submission.votes += 1
     submission.save()
-    if submission.votes>=10:
+    if submission.votes >= 10:
         add_to_story(submission)
-    return(redirect(main, page))
+    return redirect(main, page)
+
 
 def dislike_button(request, page, submission_id):
     submission = Submission.objects.get(pk=submission_id)
-    if submission.votes <=0:
+    if submission.votes <= 0:
         pass
     else:
-        submission.votes=submission.votes-1
+        submission.votes -= 1
         submission.save()
-    return(redirect(main, page))
+    return redirect(main, page)
 
-def add_to_story (submission):
-    submission.story.body= submission.story.body+ '\n'+ (submission.text)
+
+def add_to_story(submission):
+    submission.story.body = submission.story.body + '\n' + submission.text
     submission.story.save()
     submission.delete()
